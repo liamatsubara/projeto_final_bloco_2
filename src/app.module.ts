85ddbfe224015459246fad495b3520d0c9670893
perkles <matsubaralia@gmail.com>
2025-01-28 10:51:13 -0300

    :sparkles: Recurso Categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from './categoria/categoria.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -13,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     synchronize: true,
     logging: true
   }),
+  CategoriaModule
   ],
   controllers: [],
   providers: [],
